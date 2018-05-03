9be620352a94cf5a126a69d9b788d5649d92a34c
Gregory Manley <gregory.f.manley@gmail.com>
2018-05-03 13:29:13 -0400

    Create config.js
    for mining pool turtle

---
+++ config.js
@@ -0,0 +1,26 @@
+/* Must point to the API setup in your config.json file. */
+var api = "http://poolhost:8117";
+
+/* Pool server host to instruct your miners to point to.  */
+var poolHost = "192.168.1.117";
+
+/* IRC Server and room used for embedded KiwiIRC chat. */
+var irc = "";
+
+/* Contact email address. */
+var email = "";
+
+/* Market stat display params from https://www.cryptonator.com/widget */
+var cryptonatorWidget = ["TRTL-BTC", "TRTL-USD", "TRTL-EUR"];
+
+/* Download link to cryptonote-easy-miner for Windows users. */
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+/* Used for front-end block links. */
+var blockchainExplorer = "http://chainradar.com/{symbol}/block/{id}";
+
+/* Used by front-end transaction links. */
+var transactionExplorer = "http://chainradar.com/{symbol}/transaction/{id}";
+
+/* Any custom CSS theme for pool frontend */
+var themeCss = "themes/default-theme.css";
